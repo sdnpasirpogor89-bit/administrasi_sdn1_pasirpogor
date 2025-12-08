4a5799c6e85a516071eb94ea7671e9432eb4a0f0
sdnpasirpogor89-bit <email_akun_sdnpasirpogor@gmail.com>
2025-12-08 08:15:06 +0700

    update terbaru

--- src/attendance-teacher/LocationValidator.js
+++ src/attendance-teacher/LocationValidator.js
@@ -12,7 +12,7 @@ const SCHOOL_COORDS = {
   lng: 107.393952,
 };
 
-const SCHOOL_RADIUS = 200; // 200 meter radius
+const SCHOOL_RADIUS = 100; // 200 meter radius
 
 // Debug mode - set true untuk lihat detail GPS di console
 const DEBUG_MODE = true;
